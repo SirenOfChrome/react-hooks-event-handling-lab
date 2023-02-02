614099d7893536299276e0decb20189cedf16c77
Forrest Jones <forrestjones2010@gmail.com>
2023-02-02 15:17:35 -0600

    GDC#4

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Keypad from './components/Keypad';
+import EyesOnMe from './components/EyesOnMe';
+
+const App = () => {
+  return (
+    <div>
+      <Keypad />
+      <EyesOnMe />
+    </div>
+  );
+};
+
+export default App;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,18 @@
-// Code Keypad Component Here
+import React, { useState } from 'react';
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad () {
+  const [password, setPassword] = useState('');
+
+  const handleChange = event => {
+    setPassword(event.target.value);
+    console.log('Entering password...');
+  }
+
+  return (
+    <div>
+      <input type="password" onChange={handleChange} value={password} />
+    </div>
+  )
 }
 
 export default Keypad;
